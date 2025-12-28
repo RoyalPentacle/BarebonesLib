1cd497c699edb082f847c46c2d6bcac1918cbe70
RoyalPentacle <funcphysbox@gmail.com>
2025-12-28 17:17:21 +1300

    Add keybinding and localization systems.

--- BarebonesLib/Config/Control.cs
+++ BarebonesLib/Config/Control.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework.Input;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,90 @@ namespace Barebones.Config
     /// </summary>
     public static class Control
     {
+
+        private static readonly ConcurrentDictionary<string, Keys> _boundControls = new ConcurrentDictionary<string, Keys>();
+
+        /// <summary>
+        /// Loads the keybinds from the path.
+        /// Uses AddOrUpdate logic to overwrite default keybinds.
+        /// </summary>
+        public static void LoadKeybinds(string path)
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string json = sr.ReadToEnd();
+                    sr.Close();
+                    Dictionary<string, Keys> _boundTemp = JsonConvert.DeserializeObject<Dictionary<string, Keys>>(json);
+                    if (_boundTemp != null )
+                    {
+                        foreach (KeyValuePair<string, Keys> pair in _boundTemp)
+                        {
+                            _boundControls.AddOrUpdate(pair.Key, pair.Value, UpdateKey);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Verbose.WriteErrorMajor($"Failed to load control config: {path}\n  Ex: {ex.Message}");
+            }
+        }
+        private static Keys UpdateKey(string key, Keys value)
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Saves the current keybinds to the specified file.
+        /// </summary>
+        public static void SaveKeybinds(string path)
+        {
+            try
+            {
+                Directory.CreateDirectory(path);
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    string json = JsonConvert.SerializeObject(_boundControls, Formatting.Indented);
+                    sw.WriteLine(json);
+                    Verbose.WriteLogMinor($"Successfully saved controls.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Verbose.WriteErrorMajor($"Failed to save control config!\n  Ex: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Registers a new keybind to the dictionary of controls.
+        /// Only do this at launch, ideally in a function that will only register default controls if no bind exists for that control.
+        /// </summary>
+        /// <param name="control"></param>
+        /// <param name="key"></param>
+        public static void RegisterKeybind(string control, Keys key)
+        {
+            if (!_boundControls.TryAdd(control, key))
+            {
+                Verbose.WriteErrorMinor($"Failed to register keybind({control}, {key.ToString()})");
+            }
+        }
+
+        /// <summary>
+        /// Checks if the specified key associated with this string was pressed this frame.
+        /// Only if the key was not pressed the previous frame.
+        /// </summary>
+        /// <param name="key">The string to check.</param>
+        /// <returns>True if the key has just been pressed. False otherwise.</returns>
+        public static bool KeyPressed(string key)
+        {
+            if (_boundControls.TryGetValue(key, out Keys value))
+                return KeyPressed(value);
+            else
+                return false;
+        }
+
         /// <summary>
         /// Checks if the specified key was pressed this frame.
         /// Only if the key was not pressed the previous frame.
@@ -24,14 +110,42 @@ namespace Barebones.Config
         }
 
         /// <summary>
+        /// Checks if the specified key associated with this string is being held down.
+        /// Specifically, is the key being pressed at all.
+        /// </summary>
+        /// <param name="key">The string to check.</param>
+        /// <returns>True if the key is being held. False otherwise.</returns>
+        public static bool KeyHeld(string key)
+        {
+            if (_boundControls.TryGetValue(key, out Keys value))
+                return KeyHeld(value);
+            else
+                return false;
+        }
+
+        /// <summary>
         /// Checks if the specified key is being held down.
         /// Specifically, is the key being pressed at all.
         /// </summary>
         /// <param name="key">The key to check.</param>
-        /// <returns>True if the key is being pressed. False otherwise.</returns>
+        /// <returns>True if the key is being held. False otherwise.</returns>
         public static bool KeyHeld(Keys key)
         {
             return Engine.NewKeyboardState[key] == KeyState.Down;
+        }
+
+        /// <summary>
+        /// Checks if the specified key associated with this string was released this frame.
+        /// Only if the key was pressed the previous frame.
+        /// </summary>
+        /// <param name="key">The string to check.</param>
+        /// <returns>True if the key was released. False otherwise.</returns>
+        public static bool KeyReleased(string key)
+        {
+            if (_boundControls.TryGetValue(key, out Keys value))
+                return KeyReleased(value);
+            else
+                return false;
         }
 
         /// <summary>
---
+++ BarebonesLib/Config/Language.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barebones.Config
+{
+    /// <summary>
+    /// A class that contains handlers for localization of strings.
+    /// Broadly speaking. Do not change the language while a game is running. Require a restart.
+    /// </summary>
+    public static class Language
+    {
+        private static readonly ConcurrentDictionary<string, string> _localization = new ConcurrentDictionary<string, string>();
+        private static string _currentLang = "en";
+
+        /// <summary>
+        /// The current language of the game.
+        /// </summary>
+        public static string CurrentLanguage
+        {
+            get { return _currentLang; }
+        }
+
+        /// <summary>
+        /// Only do this at launch, before loading the first language file.
+        /// </summary>
+        /// <param name="lang"></param>
+        public static void SetLanguage(string lang)
+        {
+            _currentLang = lang;
+        }
+
+        /// <summary>
+        /// Loads a localization set from a file.
+        /// Appends to the existing localization dictionary if the language of the file is the same as the current.
+        /// Writes an error if it is not the same language.
+        /// </summary>
+        /// <param name="languagePath"></param>
+        public static void LoadLanguageFile(string languagePath)
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText(languagePath))
+                {
+                    string json = sr.ReadToEnd();
+                    sr.Close();
+                    Dictionary<string, string> _tempLocal = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                    if (_tempLocal != null)
+                    {
+                        foreach (KeyValuePair<string, string> pair in _tempLocal)
+                        {
+                            _localization.AddOrUpdate(pair.Key, pair.Value, UpdateString);
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Verbose.WriteErrorMajor($"Failed to load localization file: {languagePath}\n Ex: {ex.Message}");
+            }
+        }
+
+        private static string UpdateString(string key, string value)
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Gets the corresponding string to the given localization token.
+        /// </summary>
+        /// <param name="token">The token to translate.</param>
+        /// <returns>The translated string, or the token if no string exists.</returns>
+        public static string Translate(string token)
+        {
+            if (_localization.TryGetValue(token, out var result))
+            {
+                return result;
+            }
+            else
+            {
+                Verbose.WriteErrorMinor($"Missing localization string for token: {token}");
+                return token;
+            }
+        }
+    }
+}
